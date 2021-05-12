cb5e9d167804ded6a22aa689e36b1a019e93ff9e
MahAmorim <61316782+MahAmorim@users.noreply.github.com>
2021-05-12 20:32:04 -0300

    Add files via upload

---
+++ NumerosPares.java
@@ -0,0 +1,32 @@
+
+/**********************************************************************************************
+ *
+ *DESAFIO: Exibição de Sequencia de Números Pares até n
+ *
+ *Crie um programa que leia um número e mostre os números pares até esse número, inclusive 
+ * ele mesmo.
+ *
+ *Entrada: Você receberá 1 valor inteiro N, onde N > 0.
+ *Saída: Exiba todos os números pares até o valor de entrada, sendo um em cada linha. 
+ *
+ * Exemplo: entrada = 6 -> exiba 2 4 6
+ **********************************************************************************************/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ExibicaoNumerosPares {
+
+  public static void main(String[] args) throws IOException {
+
+    String entrada;
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    entrada = br.readLine();
+    for (int i = 2; i <= Integer.parseInt(entrada); i++) {
+      if (i % 2 == 0) {
+        System.out.println(i);
+      }
+    }
+  }
+}
